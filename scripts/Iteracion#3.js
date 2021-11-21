dcdf6e755bd7e5b7cfc4a5b75e3962594e40c438
gutijaume <gutijaume@hotmail.com>
2021-11-21 12:55:39 +0100

    Create Iteracion#3.js
    Creación de Iteracion#3

---
+++ scripts/Iteracion#3.js
@@ -0,0 +1,32 @@
+//3.1//
+let a = 10;
+let b = 5;
+let total = 0;
+total = a * b;
+alert("El resultado es: " + total);
+
+//3.2//
+let a = 10;
+let b = 2;
+let total = 0;
+total = a / b;
+alert(total);
+
+//3.3//
+let a = 15;
+let b = 9;
+let total = 0;
+total = a % b;
+alert(total);
+
+//3.4//
+let y = 10;
+let z = 5;
+let x = y + z;
+console.log(x)
+
+//3.5//
+y = 10;
+z = 5;
+x = y * z;
+console.log(x)
