4f82b07b7366e5a3679d6c78461420c382bd6e0c
gutijaume <gutijaume@hotmail.com>
2021-11-28 16:47:20 +0100

    Create Iteracion#6.js

---
+++ scripts/Iteracion#6.js
@@ -0,0 +1,23 @@
+//BUCLES//
+
+//6.1//
+for (let i = 0; i<=9; i++){
+    console.log(i);
+};
+
+//6.2//
+
+for (let i = 0; i<=9; i++){
+    if (i%2 === 0){
+        console.log(i)
+    }
+};
+
+//6.3//
+for (let i=0; i <= 10; i++){
+    if (i<10){
+        console.log("Intenando dormir")
+    } else {
+        console.log("Dormido!")
+    }
+}
